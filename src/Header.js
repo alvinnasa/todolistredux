fe544dad9c586dfd3d46c5db3f9de4bea6482180
Alvinnasa Bada Chusnul <alvinnasa@seventech.id>
2022-03-07 15:14:46 +0700

    First

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+const Header = () => {
+   return (
+       <header>
+           <h1>To Do List</h1>
+           <a>klik untuk selesaikan tugas</a>
+           <br/>
+           <a>---------------------------</a>
+       </header>
+   );
+};
+ 
+export default Header;
